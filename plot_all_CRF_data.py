c453f81e2819564a7714a390f7bb0a2ff4637574
Lauri Nurminen <larsnurminen@gmail.com>
2018-01-15 17:01:31 -0700

    Add data selection and started plotter

--- plot_all_CRF_data.py
+++ plot_all_CRF_data.py
@@ -7,13 +7,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tables as tb
+import matplotlib.pyplot as plt
 
 animal  = 'MM378'
 #penetrs = np.array([2,3,4,6])
 penetrs = np.array([2])
 root_folder = '/opt3/' + animal + '/'
 
-
+laser_amps = []
 # iterate penetrations
 for p in penetrs:
     # open the HDF5-file that is collated over all penetrations
@@ -23,10 +24,33 @@ for p in penetrs:
     for u in data_file.iter_nodes('/MM378/P'+str(p)):
         # iterate through datasets as not all datasets are necessarily measured for
         # each unit
-        for ds in u.keys():
-            # if the experiment was contrast then do plotting
-            if u[ds] == 'contrast':
-                # run cool analysis
+        for ds in u._v_children.keys():
+            # plot if the experiment was contrast then do plotting
+            if u._v_children[ds].attrs['experiment'] == 'contrast':
+                # gather data
+                spikeCounts_NoL = np.squeeze(u._v_children[ds].col('spikeCounts_NoL'))
+                spikeCounts_L   = np.squeeze(u._v_children[ds].col('spikeCounts_L'))
+                spikeCounts_NoL_mean = np.mean(spikeCounts_NoL,axis=1)
+                spikeCounts_L_mean   = np.mean(spikeCounts_L,axis=1)
+                spikeCounts_NoL_SE = np.std(spikeCounts_NoL,axis=1)/np.sqrt(spikeCounts_NoL.shape[1])
+                spikeCounts_L_SE   = np.std(spikeCounts_L,axis=1)/np.sqrt(spikeCounts_L.shape[1])
                 
-        
-
+                if u._v_children[ds].attrs['laser_powr'] == 800 and u._v_children[ds].attrs['stim_size'] == 1.2:
+                    plt.subplot(331)
+                    plt.errorbar(u._v_children[ds].attrs['vals'],spikeCounts_NoL_mean,yerr=spikeCounts_NoL_SE,fmt='go-')
+                elif u._v_children[ds].attrs['laser_powr'] == 800 and u._v_children[ds].attrs['stim_size'] == 2.4:
+                    plt.subplot(332)
+                elif u._v_children[ds].attrs['laser_powr'] == 800 and u._v_children[ds].attrs['stim_size'] == 3.6:
+                    plt.subplot(333)
+                elif u._v_children[ds].attrs['laser_powr'] == 900 and u._v_children[ds].attrs['stim_size'] == 1.2:
+                    plt.subplot(334)
+                elif u._v_children[ds].attrs['laser_powr'] == 900 and u._v_children[ds].attrs['stim_size'] == 2.4:
+                    plt.subplot(335)
+                elif u._v_children[ds].attrs['laser_powr'] == 900 and u._v_children[ds].attrs['stim_size'] == 3.6:
+                    plt.subplot(336)
+                elif u._v_children[ds].attrs['laser_powr'] == 1000 and u._v_children[ds].attrs['stim_size'] == 1.2:
+                    plt.subplot(337)
+                elif u._v_children[ds].attrs['laser_powr'] == 1000 and u._v_children[ds].attrs['stim_size'] == 2.4:
+                    plt.subplot(338)
+                elif u._v_children[ds].attrs['laser_powr'] == 1000 and u._v_children[ds].attrs['stim_size'] == 3.6:
+                    plt.subplot(339)
